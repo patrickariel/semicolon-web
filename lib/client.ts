f0b3657716ee9736d3da4de2057503c2d9f379bb
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-06-15 13:12:14 +0700

    refactor: change environment variable for base API url

--- lib/client.ts
+++ lib/client.ts
@@ -2,6 +2,6 @@ import { contract } from "@semicolon/api";
 import { initQueryClient } from "@ts-rest/react-query";
 
 export const client = initQueryClient(contract, {
-  baseUrl: process.env.API_URL ?? "http://localhost:4000",
+  baseUrl: process.env.API_BASE_URL ?? "http://localhost:4000",
   validateResponse: true,
 });
--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   transpilePackages: ["@semicolon/api"],
   env: {
-    API_URL: process.env.API_URL,
+    API_URL: process.env.API_BASE_URL,
   },
 };
 
