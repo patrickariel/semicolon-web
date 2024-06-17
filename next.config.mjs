1bd58c1a25c3818dec8ba74e3bcb19c9a73acc07
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-06-17 12:49:50 +0700

    chore: bump @semicolon/api

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["@semicolon/api"],
   env: {
     API_BASE_URL: process.env.API_BASE_URL ?? "http://localhost:4000/trpc",
   },
