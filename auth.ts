a643fcdd0121f600057c89f7c25947607029fe59
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-06-19 21:38:04 +0700

    feat: add Discord, GitHub and Google as OAuth providers

--- auth.ts
+++ auth.ts
@@ -1,10 +1,17 @@
 import { TrpcAdapter } from "./adapter";
 import NextAuth from "next-auth";
+import Discord from "next-auth/providers/discord";
+import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 import Resend from "next-auth/providers/resend";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google, Resend({ from: "onboarding@resend.dev" })],
+  providers: [
+    Resend({ from: "onboarding@resend.dev" }),
+    Google,
+    Discord,
+    GitHub,
+  ],
   adapter: TrpcAdapter,
   session: { strategy: "jwt" },
 });
