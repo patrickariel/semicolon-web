f3ef0306e17ebd38354bba4dab162de13c87f642
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-06-20 20:59:04 +0700

    feat: improve loading for front page

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import Spinner from "@/components/ui/spinner";
 import { trpc } from "@/lib/trpc-client";
 import { redirect } from "next/navigation";
 
@@ -15,7 +16,7 @@ export default function Page() {
   const { data, isFetching, isError } = trpc.user.me.useQuery(undefined, {
     retry: (_failureCount, error) => {
       if (
-        error.data?.code === "FORBIDDEN" ||
+        error.data?.code === "UNAUTHORIZED" ||
         error.data?.code === "PRECONDITION_FAILED"
       ) {
         return false;
@@ -24,28 +25,28 @@ export default function Page() {
     },
   });
 
-  if (isFetching) {
-    return <div>loading</div>;
-  }
-
-  if (isError || !data) {
+  if (isError) {
     redirect("/flow/signup");
   }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <Card className="h-[200px] w-[300px]">
-        <CardHeader>
-          <CardTitle>{data.name}</CardTitle>
-          <CardDescription>{data.username}</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>{data.bio}</p>
-        </CardContent>
-        <CardFooter>
-          <p className="text-xs text-muted-foreground">{data.id}</p>
-        </CardFooter>
-      </Card>
+      {!isFetching && data ? (
+        <Card className="h-[200px] w-[300px]">
+          <CardHeader>
+            <CardTitle>{data.name}</CardTitle>
+            <CardDescription>{data.username}</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>{data.bio}</p>
+          </CardContent>
+          <CardFooter>
+            <p className="text-xs text-muted-foreground">{data.id}</p>
+          </CardFooter>
+        </Card>
+      ) : (
+        <Spinner />
+      )}
     </main>
   );
 }
