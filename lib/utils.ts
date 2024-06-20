d1c87f22bc0032a2e4cd78644a1eea08460cdf8f
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-06-21 00:16:05 +0700

    fix: repair API communications for production
    It looks like that tRPC is not working in production, for various
    bizzare reasons. So here are my thoughts and hacks.
    
    For client-side API calls, we will use Next.js rewrites to make it
    easier to pass cross-origin cookies. According to this SE post:
    https://stackoverflow.com/a/71902594/10625876, cookies can only be
    passed among the same domain (but different ports) even with configured
    CORS. It's unsourced and rather suspect, but it seems to fit my
    experience. I just can't figure out a way to make cross-origin cookies
    work during production.
    
    For server-side API calls, we will call the API's domain directly.
    I have actually tried three ways of doing this:
    - Call $VERCEL_URL
    - Call the actual public production domain
    - Call the API's domain directly
    But only the latter two works. The first one somehow returns an HTML
    document. Again, I can't figure out why. But it might be a
    Vercel-specific quirk.

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,19 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function getBaseUrl() {
+  if (typeof window !== "undefined") {
+    return "";
+  }
+
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
+  }
+
+  if (process.env.RENDER_INTERNAL_HOSTNAME) {
+    return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
+  }
+
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+}
