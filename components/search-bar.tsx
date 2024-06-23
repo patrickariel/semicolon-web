5e6dc90fd2af27ae14c636fddd97ca16e9f70a1b
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-06-24 01:55:07 +0700

    feat: improve search bar border

--- components/search-bar.tsx
+++ components/search-bar.tsx
@@ -3,13 +3,13 @@ import { Search } from "lucide-react";
 import React from "react";
 
 const SearchBar = () => (
-  <div className="flex items-center justify-start rounded-full border pl-5">
+  <div className="flex items-center justify-start gap-4 rounded-full border pl-4">
     <Search />
     <Input
       type="text"
       placeholder="Search"
       id="search"
-      className="h-12 flex-grow border-none bg-transparent text-base text-white"
+      className="h-12 flex-grow rounded-r-full border-none bg-transparent text-base text-white"
     />
   </div>
 );
