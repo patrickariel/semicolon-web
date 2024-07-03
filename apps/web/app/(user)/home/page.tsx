31cdeb69e96801f7f3df9ef040b7983142fd4db4
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-07-03 22:51:49 +0700

    feat(web): return latest tweets on homepage

--- apps/web/app/(user)/home/page.tsx
+++ apps/web/app/(user)/home/page.tsx
@@ -3,11 +3,18 @@
 import { FeedHeader } from "@/components/feed-header";
 import { PostForm } from "@/components/post-form";
 import { Tweet } from "@/components/tweet";
+import { trpc } from "@/lib/trpc-client";
 import { Separator } from "@semicolon/ui/separator";
 import _ from "lodash";
 import React, { Fragment } from "react";
 
 export default function Page() {
+  const { data } = trpc.post.search.useQuery({});
+
+  if (!data) {
+    return;
+  }
+
   return (
     <div className="flex flex-col">
       <div className="bg-card sticky top-0 z-50">
@@ -17,21 +24,9 @@ export default function Page() {
       <PostForm />
       <Separator />
       <div className="mb-4 flex flex-col">
-        <Tweet
-          username="i_have_a_really_long_username_and_i_cannot_lie"
-          date="2024-06-21"
-          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend libero id facilisis scelerisque. Nulla nec lectus tristique, ultricies felis sed, suscipit neque."
-          feeling="🤩 happy"
-        />
-        <Separator />
-        {_.range(0, 16).map((i) => (
+        {data.results.map((tweet, i) => (
           <Fragment key={i}>
-            <Tweet
-              username="example_user"
-              date="2024-06-21"
-              content="This is a sample tweet content."
-              feeling="🤩 happy"
-            />
+            <Tweet {...tweet} />
             <Separator />
           </Fragment>
         ))}
