8f31aee576cc9c192fd97da606ddbc5b5ef08dc6
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-07-03 22:06:38 +0700

    feat(web): add UUID shortener

--- apps/web/lib/utils.ts
+++ apps/web/lib/utils.ts
@@ -1,3 +1,7 @@
+import short from "short-uuid";
+
+export const uuidTranslator = short(short.constants.flickrBase58);
+
 export function getBaseUrl() {
   if (typeof window !== "undefined") {
     return "";
