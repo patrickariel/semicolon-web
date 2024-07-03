2c859ab557a037b8db70d57d0918549feec762af
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-07-03 15:25:37 +0700

    feat(db): add kysely for raw queries

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,3 +1,39 @@
-export type { User } from "@prisma/client";
-export { PrismaClient, Prisma } from "@prisma/client";
+import type { DB } from "../prisma/generated/types";
+import { PrismaClient } from "@prisma/client";
+import {
+  Kysely,
+  PostgresAdapter,
+  PostgresIntrospector,
+  PostgresQueryCompiler,
+} from "kysely";
+import kyselyExtension from "prisma-extension-kysely";
+
+export type {
+  Prisma,
+  PrismaPromise,
+  User,
+  Post,
+  Account,
+  Like,
+  Media,
+  Session,
+  VerificationToken,
+} from "@prisma/client";
+export { PrismaClient } from "@prisma/client";
 export { UserSchema } from "@semicolon/db/zod";
+
+const prisma = new PrismaClient();
+
+export const db = prisma.$extends(
+  kyselyExtension({
+    kysely: (driver) =>
+      new Kysely<DB>({
+        dialect: {
+          createDriver: () => driver,
+          createAdapter: () => new PostgresAdapter(),
+          createIntrospector: (db) => new PostgresIntrospector(db),
+          createQueryCompiler: () => new PostgresQueryCompiler(),
+        },
+      }),
+  }),
+);
