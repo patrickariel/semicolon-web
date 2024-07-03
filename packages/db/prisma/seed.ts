1b2b0cc5ef074000c9258bcd59f52d6962356849
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-07-03 15:21:50 +0700

    feat(db): add more seed data

--- packages/db/prisma/seed.ts
+++ packages/db/prisma/seed.ts
@@ -1,4 +1,6 @@
-import { PrismaClient } from "@prisma/client";
+import { faker } from "@faker-js/faker";
+import { Prisma, PrismaClient } from "@prisma/client";
+import _ from "lodash";
 
 const prisma = new PrismaClient();
 
@@ -11,6 +13,71 @@ async function main() {
       email: "john.smith@example.com",
       bio: "Software developer.",
     },
+  });
+
+  const users = await prisma.user.createManyAndReturn({
+    data: _.range(0, 50).map(() => ({
+      name: faker.person.fullName(),
+      username: faker.internet.userName(),
+      email: faker.internet.email(),
+      bio: faker.person.bio(),
+    })),
+  });
+
+  const posts = await prisma.post.createManyAndReturn({
+    data: _.flattenDeep(
+      users.map((user) =>
+        _.range(0, 25).map((): Prisma.PostCreateManyAndReturnArgs["data"] => ({
+          userId: user.id,
+          content: faker.lorem.paragraph({
+            min: 1,
+            max: 4,
+          }),
+          createdAt: faker.date.between({
+            from: "2020-01-01",
+            to: "2024-01-01",
+          }),
+        })),
+      ),
+    ),
+  });
+
+  const replies = await prisma.post.createManyAndReturn({
+    data: _.flattenDeep(
+      posts.map((post) =>
+        _.range(0, 5).map((): Prisma.PostCreateManyAndReturnArgs["data"] => ({
+          parentId: post.id,
+          userId: _.sample(users)!.id, // eslint-disable-line @typescript-eslint/no-non-null-assertion
+          content: faker.lorem.paragraph({
+            min: 1,
+            max: 4,
+          }),
+          createdAt: faker.date.between({
+            from: post.createdAt,
+            to: "2024-01-01",
+          }),
+        })),
+      ),
+    ),
+  });
+
+  const _moreReplies = await prisma.post.createManyAndReturn({
+    data: _.flattenDeep(
+      replies.map((reply) =>
+        _.range(0, 5).map((): Prisma.PostCreateManyAndReturnArgs["data"] => ({
+          parentId: reply.id,
+          userId: _.sample(users)!.id, // eslint-disable-line @typescript-eslint/no-non-null-assertion
+          content: faker.lorem.paragraph({
+            min: 1,
+            max: 4,
+          }),
+          createdAt: faker.date.between({
+            from: reply.createdAt,
+            to: "2024-01-01",
+          }),
+        })),
+      ),
+    ),
   });
 }
 
