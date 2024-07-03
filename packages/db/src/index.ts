97a714d157f594baabc670418ca5bc4cb9f2db45
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-07-03 20:11:01 +0700

    feat(db): update entry point

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -2,7 +2,6 @@ import type { DB } from "../prisma/generated/types";
 import { PrismaClient } from "@prisma/client";
 import chalk from "chalk";
 import { highlight } from "cli-highlight";
-import figures from "figures";
 import {
   Kysely,
   PostgresAdapter,
@@ -17,7 +16,6 @@ export type {
   User,
   Post,
   Account,
-  Like,
   Media,
   Session,
   VerificationToken,
@@ -26,7 +24,8 @@ export { PrismaClient } from "@prisma/client";
 export { UserSchema } from "@semicolon/db/zod";
 
 const prisma = new PrismaClient(
-  process.env.NODE_ENV === "development" && process.env.PRISMA_LOG_QUERY
+  (process.env.NODE_ENV ?? "development") === "development" &&
+  process.env.PRISMA_LOG_QUERY
     ? {
         log: [
           {
@@ -38,7 +37,11 @@ const prisma = new PrismaClient(
     : undefined,
 );
 
-if (process.env.NODE_ENV === "development" && process.env.PRISMA_LOG_QUERY) {
+if (
+  (process.env.NODE_ENV ?? "development") === "development" &&
+  process.env.PRISMA_LOG_QUERY
+) {
+  console.log("logging");
   prisma.$on("query", (e) => {
     const query = highlight(e.query, { language: "sql", ignoreIllegals: true });
     const params = highlight(e.params, {
@@ -46,7 +49,7 @@ if (process.env.NODE_ENV === "development" && process.env.PRISMA_LOG_QUERY) {
       ignoreIllegals: true,
     });
     console.log(
-      `QUERY ${query} ${chalk.green(figures.arrowLeft)} ${params} in ${chalk.yellow(`${e.duration.toString()}ms`)}`,
+      `QUERY ${query} ${chalk.green("<==")} ${params} in ${chalk.yellow(`${e.duration.toString()}ms`)}`,
     );
   });
 }
