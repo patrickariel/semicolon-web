4554e587bed726616efd83058ae04f1b9a32052b
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-07-03 16:15:30 +0700

    feat(db): add query logging

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,5 +1,8 @@
 import type { DB } from "../prisma/generated/types";
 import { PrismaClient } from "@prisma/client";
+import chalk from "chalk";
+import { highlight } from "cli-highlight";
+import figures from "figures";
 import {
   Kysely,
   PostgresAdapter,
@@ -22,7 +25,31 @@ export type {
 export { PrismaClient } from "@prisma/client";
 export { UserSchema } from "@semicolon/db/zod";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient(
+  process.env.NODE_ENV === "development" && process.env.PRISMA_LOG_QUERY
+    ? {
+        log: [
+          {
+            emit: "event",
+            level: "query",
+          },
+        ],
+      }
+    : undefined,
+);
+
+if (process.env.NODE_ENV === "development" && process.env.PRISMA_LOG_QUERY) {
+  prisma.$on("query", (e) => {
+    const query = highlight(e.query, { language: "sql", ignoreIllegals: true });
+    const params = highlight(e.params, {
+      language: "json",
+      ignoreIllegals: true,
+    });
+    console.log(
+      `QUERY ${query} ${chalk.green(figures.arrowLeft)} ${params} in ${chalk.yellow(`${e.duration.toString()}ms`)}`,
+    );
+  });
+}
 
 export const db = prisma.$extends(
   kyselyExtension({
