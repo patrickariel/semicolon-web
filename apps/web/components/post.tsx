ba72fa29a247a4a7d0546f58a52ad3bf96b7551d
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-07-04 17:12:54 +0700

    feat(web): make button highlight customizable

--- apps/web/components/post.tsx
+++ apps/web/components/post.tsx
@@ -3,6 +3,8 @@ import { Slot } from "@radix-ui/react-slot";
 import type { PostResolved } from "@semicolon/api/schema";
 import { Avatar, AvatarFallback, AvatarImage } from "@semicolon/ui/avatar";
 import { Button, ButtonProps } from "@semicolon/ui/button";
+import { cn } from "@semicolon/ui/utils";
+import { cva, type VariantProps } from "class-variance-authority";
 import {
   BadgeCheck,
   BarChart2,
@@ -31,7 +33,23 @@ type PostButtonProps = ButtonProps &
   ) & {
     icon: LucideIcon;
     label?: string;
-  };
+  } & VariantProps<typeof highlightVariants>;
+
+const highlightVariants = cva([], {
+  variants: {
+    highlight: {
+      blue: "hover:bg-blue-400/20 [&>*]:group-hover:stroke-blue-400 [&~*]:group-hover:text-blue-400",
+      pink: "hover:bg-pink-500/20 [&>*]:group-hover:stroke-pink-500 [&~*]:group-hover:text-pink-500",
+      green:
+        "hover:bg-green-600/20 [&>*]:group-hover:stroke-green-600 [&~*]:group-hover:text-green-600",
+      yellow:
+        "hover:bg-yellow-400/20 [&>*]:group-hover:stroke-yellow-400 [&~*]:group-hover:text-yellow-400",
+    },
+  },
+  defaultVariants: {
+    highlight: "blue",
+  },
+});
 
 export function PostButton({
   icon: Icon,
@@ -39,6 +57,8 @@ export function PostButton({
   children,
   href,
   onClick,
+  className,
+  highlight,
   ...props
 }: PostButtonProps) {
   const Comp = href
@@ -53,7 +73,10 @@ export function PostButton({
     <div className="group relative flex flex-row items-center">
       <Button
         variant={"ghost"}
-        className="flex h-8 items-center justify-start gap-0 rounded-full p-2 after:absolute after:inset-0 after:block after:content-[''] hover:bg-blue-400/20"
+        className={cn(
+          highlightVariants({ highlight, className }),
+          "flex h-8 items-center justify-start gap-0 rounded-full p-2 after:absolute after:inset-0 after:block after:content-['']",
+        )}
         asChild={href ? true : false}
         onClick={(e) => {
           onClick?.(e);
@@ -62,14 +85,10 @@ export function PostButton({
         {...props}
       >
         <Comp>
-          <Icon className="stroke-muted-foreground size-[1.1rem] group-hover:stroke-blue-400" />
+          <Icon className={`stroke-muted-foreground size-[1.1rem]`} />
         </Comp>
       </Button>
-      {label && (
-        <p className="text-muted-foreground text-xs group-hover:text-blue-400">
-          {label}
-        </p>
-      )}
+      {label && <p className={`text-muted-foreground text-xs`}>{label}</p>}
       {children}
     </div>
   );
@@ -164,8 +183,18 @@ export function Post({
             href={`/post/${shortId}`}
             label="15"
           />
-          <PostButton icon={Repeat2} onClick={() => undefined} label="15" />
-          <PostButton icon={Heart} onClick={() => undefined} label="15" />
+          <PostButton
+            icon={Repeat2}
+            highlight="green"
+            onClick={() => undefined}
+            label="15"
+          />
+          <PostButton
+            icon={Heart}
+            highlight="pink"
+            onClick={() => undefined}
+            label="15"
+          />
           <PostButton icon={BarChart2} onClick={() => undefined} label="15" />
           <PostButton icon={Upload} onClick={() => undefined} />
         </div>
