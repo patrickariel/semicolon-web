2453a669961044033b1c4e12b1179575a46f1a39
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-07-05 18:36:43 +0700

    feat(web): show actual user information in profile

--- apps/web/app/(user)/[username]/page.tsx
+++ apps/web/app/(user)/[username]/page.tsx
@@ -2,7 +2,9 @@
 
 import { Post } from "@/components/post";
 import ProfileCard from "@/components/profile-card";
+import { trpc } from "@/lib/trpc-client";
 import { Separator } from "@semicolon/ui/separator";
+import Spinner from "@semicolon/ui/spinner";
 import _ from "lodash";
 import React from "react";
 
@@ -11,40 +13,48 @@ export default function Page({
 }: {
   params: { username: string };
 }) {
-  const isOwner = true; // Replace with actual logic to check if logged-in user is the profile owner
-  const isFollowing = false; // Replace with actual logic to check if logged-in user is following the profile
+  const { data: user } = trpc.user.username.useQuery({ username });
+  const { data: posts } = trpc.post.search.useQuery({
+    from: username,
+    reply: false,
+  });
+
+  if (!user) {
+    return (
+      <div className="flex min-h-20 items-center justify-center">
+        <Spinner size={30} />
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-col">
       <ProfileCard
-        name="example_user"
+        name={user.name}
         username={username}
         bio="Newbie coder, trying to be better."
-        location="Indoensia"
-        following={122}
-        followers={17273}
+        following={user.following}
+        followers={user.followers}
         coverImage="https://cdn.pixabay.com/photo/2017/08/05/12/58/computer-2583383_1280.jpg"
-        profileImage="https://avatars.githubusercontent.com/u/28171661"
+        profileImage={user.image}
+        location="Europe"
         createdAt={new Date("2024-02-01")}
         verified={true}
-        isOwner={isOwner}
-        isFollowing={isFollowing}
+        isOwner={true}
+        isFollowing={false}
       />
       <Separator />
       <div className="mb-4 flex flex-col">
-        {_.range(0, 16).map((index) => (
-          <React.Fragment key={index}>
-            <Post
-              id=""
-              name=""
-              avatar={null}
-              username="example_user"
-              createdAt={new Date()}
-              content="This is a sample tweet content."
-            />
-            <Separator />
-          </React.Fragment>
-        ))}
+        {posts ? (
+          posts.results.map((post, i) => (
+            <React.Fragment key={i}>
+              <Post {...post} />
+              <Separator />
+            </React.Fragment>
+          ))
+        ) : (
+          <Spinner size={30} />
+        )}
       </div>
     </div>
   );
