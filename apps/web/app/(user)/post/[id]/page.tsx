5014588a225294a1fe161e8b319811754e182203
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-07-05 22:57:49 +0700

    feat(web): improve post detail loading

--- apps/web/app/(user)/post/[id]/page.tsx
+++ apps/web/app/(user)/post/[id]/page.tsx
@@ -6,15 +6,20 @@ import { Post } from "@/components/post";
 import { PostDetail } from "@/components/post-detail";
 import { trpc } from "@/lib/trpc-client";
 import { Separator } from "@semicolon/ui/separator";
+import Spinner from "@semicolon/ui/spinner";
 import _ from "lodash";
 import React, { Fragment } from "react";
 
 export default function Page({ params: { id } }: { params: { id: string } }) {
   const { data: tweet } = trpc.post.id.useQuery({ id });
-  const { data } = trpc.post.replies.useQuery({ id });
+  const { data: replies } = trpc.post.replies.useQuery({ id });
 
-  if (!tweet || !data) {
-    return;
+  if (!tweet) {
+    return (
+      <div className="flex min-h-20 items-center justify-center">
+        <Spinner size={30} />
+      </div>
+    );
   }
 
   return (
@@ -24,12 +29,18 @@ export default function Page({ params: { id } }: { params: { id: string } }) {
       <CommentForm />
       <Separator />
       <div className="mb-4 flex flex-col">
-        {data.replies.map((post, i) => (
-          <Fragment key={i}>
-            <Post {...post} />
-            <Separator />
-          </Fragment>
-        ))}
+        {replies ? (
+          replies.replies.map((post, i) => (
+            <Fragment key={i}>
+              <Post {...post} />
+              <Separator />
+            </Fragment>
+          ))
+        ) : (
+          <div className="flex min-h-20 items-center justify-center">
+            <Spinner size={30} />
+          </div>
+        )}
       </div>
     </div>
   );
