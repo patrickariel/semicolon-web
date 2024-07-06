222004a5ff388c7cef0dfd5a67380921d26f4b1f
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-07-06 20:08:37 +0700

    refactor(web): inline post header component

--- apps/web/app/(columns)/(user)/post/[id]/page.tsx
+++ apps/web/app/(columns)/(user)/post/[id]/page.tsx
@@ -1,16 +1,19 @@
 "use client";
 
 import { CommentForm } from "@/components/comment-form";
-import { HeaderWithBackButton } from "@/components/header-detail-tweet";
 import { Post } from "@/components/post";
 import { PostDetail } from "@/components/post-detail";
 import { trpc } from "@/lib/trpc-client";
+import { Button } from "@semicolon/ui/button";
 import { Separator } from "@semicolon/ui/separator";
 import Spinner from "@semicolon/ui/spinner";
 import _ from "lodash";
+import { ArrowLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
 import React, { Fragment } from "react";
 
 export default function Page({ params: { id } }: { params: { id: string } }) {
+  const router = useRouter();
   const { data: tweet } = trpc.post.id.useQuery({ id });
   const { data: replies } = trpc.post.replies.useQuery({ id });
 
@@ -24,7 +27,15 @@ export default function Page({ params: { id } }: { params: { id: string } }) {
 
   return (
     <div>
-      <HeaderWithBackButton />
+      <Button
+        variant="ghost"
+        size="icon"
+        className="rounded-full p-2"
+        onClick={() => router.back()}
+      >
+        <ArrowLeft className="size-6" />
+      </Button>
+      <h3 className="text-xl font-semibold">Posts</h3>
       <PostDetail {...tweet} />
       <CommentForm />
       <Separator />
