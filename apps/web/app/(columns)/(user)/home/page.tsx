589c30d394218436abb9976c3549343d884962a1
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-07-09 22:56:19 +0700

    refactor(web): separate infinite scroller into a component

--- apps/web/app/(columns)/(user)/home/page.tsx
+++ apps/web/app/(columns)/(user)/home/page.tsx
@@ -1,20 +1,14 @@
 "use client";
 
 import { NavTab, NavTabItem } from "@/components/nav-tab";
-import { Post } from "@/components/post";
+import { PostFeed } from "@/components/post-feed";
 import { PostForm } from "@/components/post-form";
 import { trpc } from "@/lib/trpc-client";
-import { ExclamationTriangleIcon } from "@radix-ui/react-icons";
 import type { PostResolved } from "@semicolon/api/schema";
-import { Alert, AlertDescription, AlertTitle } from "@semicolon/ui/alert";
-import { Button } from "@semicolon/ui/button";
 import { Separator } from "@semicolon/ui/separator";
-import Spinner from "@semicolon/ui/spinner";
 import _ from "lodash";
-import { RotateCw } from "lucide-react";
 import { useSession } from "next-auth/react";
-import React, { Fragment, useState } from "react";
-import { InView } from "react-intersection-observer";
+import React, { useState } from "react";
 
 export default function Page() {
   const {
@@ -48,53 +42,15 @@ export default function Page() {
         onSuccess={(post) => setMyPosts((posts) => [post, ...posts])}
       />
       <Separator />
-      {feed && (
-        <div className="flex flex-col">
-          {myPosts
-            .concat(feed.pages.flatMap((page) => page.results))
-            .map((tweet, i) => (
-              <Fragment key={i}>
-                <Post {...tweet} />
-                <Separator />
-              </Fragment>
-            ))}
-        </div>
-      )}
-      {isLoading || isFetchingNextPage ? (
-        <div className="flex h-20 items-center justify-center">
-          <Spinner size={30} />
-        </div>
-      ) : isLoadingError || (isFetchNextPageError as boolean) ? (
-        <div className="border-destructive m-5 flex flex-grow flex-row items-center justify-between rounded-lg border p-0">
-          <Alert variant="destructive" className="border-none">
-            <ExclamationTriangleIcon className="h-4 w-4" />
-            <AlertTitle>Error</AlertTitle>
-            <AlertDescription>
-              There was a problem fetching your posts.
-            </AlertDescription>
-          </Alert>
-          <Button
-            size={"icon"}
-            variant={"ghost"}
-            className="hover:bg-destructive/30 mr-4 aspect-square rounded-full"
-            onClick={async () => refetch()}
-          >
-            <RotateCw className="stroke-destructive" />
-          </Button>
-        </div>
-      ) : (
-        <InView
-          as="div"
-          threshold={0.9}
-          onChange={async (inView, _) => {
-            if (inView) {
-              await fetchNextPage();
-            }
-          }}
-        >
-          <div className="flex h-20 flex-row items-center justify-center" />
-        </InView>
-      )}
+      <PostFeed
+        posts={myPosts.concat(
+          (feed?.pages ?? []).flatMap((page) => page.results),
+        )}
+        loading={isLoading || isFetchingNextPage}
+        error={isLoadingError || isFetchNextPageError}
+        fetchNextPage={fetchNextPage}
+        refetch={refetch}
+      />
     </div>
   );
 }
---
+++ apps/web/components/post-feed.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { Post } from "@/components/post";
+import { ExclamationTriangleIcon } from "@radix-ui/react-icons";
+import type { PostResolved } from "@semicolon/api/schema";
+import { Alert, AlertDescription, AlertTitle } from "@semicolon/ui/alert";
+import { Button } from "@semicolon/ui/button";
+import { Separator } from "@semicolon/ui/separator";
+import Spinner from "@semicolon/ui/spinner";
+import _ from "lodash";
+import { RotateCw } from "lucide-react";
+import React, { Fragment } from "react";
+import { InView } from "react-intersection-observer";
+
+export function PostFeed({
+  posts,
+  loading,
+  error,
+  fetchNextPage,
+  refetch,
+}: {
+  posts: PostResolved[];
+  loading: boolean;
+  error: boolean;
+  fetchNextPage: () => Promise<unknown>;
+  refetch: () => Promise<unknown>;
+}) {
+  return (
+    <>
+      {
+        <div className="flex flex-col">
+          {posts.map((post, i) => (
+            <Fragment key={i}>
+              <Post {...post} />
+              <Separator />
+            </Fragment>
+          ))}
+        </div>
+      }
+      {loading ? (
+        <div className="flex h-20 items-center justify-center">
+          <Spinner size={30} />
+        </div>
+      ) : error ? (
+        <div className="border-destructive m-5 flex flex-grow flex-row items-center justify-between rounded-lg border p-0">
+          <Alert variant="destructive" className="border-none">
+            <ExclamationTriangleIcon className="h-4 w-4" />
+            <AlertTitle>Error</AlertTitle>
+            <AlertDescription>
+              There was a problem fetching your posts.
+            </AlertDescription>
+          </Alert>
+          <Button
+            size={"icon"}
+            variant={"ghost"}
+            className="hover:bg-destructive/30 mr-4 aspect-square rounded-full"
+            onClick={async () => refetch()}
+          >
+            <RotateCw className="stroke-destructive" />
+          </Button>
+        </div>
+      ) : (
+        <InView
+          as="div"
+          threshold={0.9}
+          onChange={async (inView, _) => {
+            if (inView) {
+              await fetchNextPage();
+            }
+          }}
+        >
+          <div className="flex h-20 flex-row items-center justify-center" />
+        </InView>
+      )}
+    </>
+  );
+}
