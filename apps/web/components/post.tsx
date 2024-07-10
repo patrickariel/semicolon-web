c8433a57dc07597abec64ef5fbe7d48d357cc6ff
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-07-10 19:00:03 +0700

    feat(web): basic reply dialog UI

--- apps/web/components/post.tsx
+++ apps/web/components/post.tsx
@@ -1,17 +1,12 @@
 import { PostButton } from "./post-button";
 import { PostDropdown } from "./post-dropdown";
+import { PostForm } from "./post-form";
 import { ThumbGrid } from "./thumb-grid";
 import { formatLongDate, formatShortDate } from "@/lib/utils";
 import type { PostResolved } from "@semicolon/api/schema";
 import { Avatar, AvatarFallback, AvatarImage } from "@semicolon/ui/avatar";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@semicolon/ui/dialog";
+import { Dialog, DialogContent, DialogTrigger } from "@semicolon/ui/dialog";
+import { Separator } from "@semicolon/ui/separator";
 import { Tooltip, TooltipContent, TooltipTrigger } from "@semicolon/ui/tooltip";
 import {
   BadgeCheck,
@@ -23,9 +18,74 @@ import {
   Upload,
   User,
 } from "lucide-react";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React from "react";
+
+function ReplyDialog(post: PostResolved) {
+  const { data: session } = useSession();
+  const { name, avatar, username, verified, createdAt, content } = post;
+
+  return (
+    <div className="flex flex-col">
+      <div className="relative flex w-full flex-row gap-3 p-3 pb-2">
+        <div className="flex flex-col items-center pt-2">
+          <Avatar className="size-11">
+            {avatar && <AvatarImage width={300} height={300} src={avatar} />}
+            <AvatarFallback>
+              <User />
+            </AvatarFallback>
+          </Avatar>
+          <Separator orientation="vertical" className="shrink border-2" />
+        </div>
+        <div className="flex w-full min-w-0 flex-col gap-2">
+          <div className="flex flex-col gap-0">
+            <div className="flex items-center justify-between gap-1">
+              <div className="flex w-full min-w-0 flex-col items-start justify-start gap-2 sm:flex-row sm:items-center">
+                <div className="flex min-w-0 max-w-full gap-1">
+                  <div className="flex min-w-0 flex-row items-center gap-1 text-sm">
+                    <p className="truncate font-bold">{name}</p>
+                    {verified && (
+                      <BadgeCheck className="size-5 flex-none stroke-sky-400" />
+                    )}
+                  </div>
+                </div>
+                <div className="flex min-w-0 max-w-full flex-row items-center gap-1">
+                  <p className="text-muted-foreground truncate align-middle text-sm">
+                    @{username}
+                  </p>
+                  <p className="text-muted-foreground align-middle text-sm">
+                    •
+                  </p>
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <p className="text-muted-foreground text-nowrap align-middle text-sm">
+                        {formatShortDate(createdAt)}
+                      </p>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>{formatLongDate(createdAt)}</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </div>
+              </div>
+            </div>
+            <p className="text-wrap text-sm leading-7">{content}</p>
+            <p className="mt-3 text-[15px]">
+              Replying to <span className="text-sky-400">@{username}</span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <PostForm
+        placeholder="Post your reply"
+        avatar={session?.user?.image}
+        className="pt-0"
+      />
+    </div>
+  );
+}
 
 export function Post(post: PostResolved) {
   const router = useRouter();
@@ -98,7 +158,7 @@ export function Post(post: PostResolved) {
                 </Link>
                 <p className="text-muted-foreground align-middle text-sm">•</p>
                 <Tooltip>
-                  <TooltipTrigger>
+                  <TooltipTrigger asChild>
                     <Link
                       href={`/${username}/post/${id}`}
                       className="text-muted-foreground text-nowrap align-middle text-sm hover:underline"
@@ -115,7 +175,7 @@ export function Post(post: PostResolved) {
             </div>
             <PostDropdown {...post} />
           </div>
-          <p className="text-wrap text-sm leading-7">{content}</p>
+          <p className="text-wrap text-[15px] leading-7">{content}</p>
         </div>
 
         {media.length > 0 && <ThumbGrid {...post} />}
@@ -126,13 +186,7 @@ export function Post(post: PostResolved) {
               <PostButton icon={MessageCircle} label={replyCount} />
             </DialogTrigger>
             <DialogContent onClick={(e) => e.stopPropagation()}>
-              <DialogHeader>
-                <DialogTitle>Are you absolutely sure?</DialogTitle>
-                <DialogDescription>
-                  This action cannot be undone. This will permanently delete
-                  your account and remove your data from our servers.
-                </DialogDescription>
-              </DialogHeader>
+              <ReplyDialog {...post} />
             </DialogContent>
           </Dialog>
           <PostButton icon={Repeat2} highlight="green" label={15} />
