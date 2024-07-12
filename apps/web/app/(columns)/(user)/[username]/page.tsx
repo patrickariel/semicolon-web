f0db964219ff670b3db945f45fc47635773b2b6f
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-07-13 01:19:33 +0700

    feat(web): functional profile tabs

--- apps/web/app/(columns)/(user)/[username]/page.tsx
+++ apps/web/app/(columns)/(user)/[username]/page.tsx
@@ -1,14 +1,17 @@
 "use client";
 
-import { Post } from "@/components/post";
 import { BackHeader } from "@/components/back-header";
+import { PostFeed } from "@/components/post-feed";
 import ProfileCard from "@/components/profile-card";
+import { myPostsAtom } from "@/lib/atom";
 import { trpc } from "@/lib/trpc-client";
-import { Separator } from "@semicolon/ui/separator";
+import type { PostResolved } from "@semicolon/api/schema";
 import Spinner from "@semicolon/ui/spinner";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@semicolon/ui/tabs";
+import { useAtomValue } from "jotai";
 import _ from "lodash";
-import React from "react";
+import { useSession } from "next-auth/react";
+import React, { useEffect, useState } from "react";
 
 enum ActiveTab {
   Posts = "posts",
@@ -17,16 +20,79 @@ enum ActiveTab {
   Likes = "likes",
 }
 
+function Posts({ username }: { username: string }) {
+  const {
+    data: feed,
+    fetchNextPage,
+    hasNextPage,
+    isLoading,
+    isLoadingError,
+    isFetchingNextPage,
+    isFetchNextPageError,
+    refetch,
+  } = trpc.user.posts.useInfiniteQuery(
+    { username, maxResults: 15 },
+    { getNextPageParam: (lastPage) => lastPage.nextCursor },
+  );
+  const { data: session } = useSession();
+
+  const myPosts = useAtomValue(myPostsAtom);
+  const [feedArray, setFeedArray] = useState<PostResolved[]>([]);
+
+  useEffect(() => {
+    setFeedArray((feed?.pages ?? []).flatMap((page) => page.posts));
+  }, [myPosts, feed, session, username]);
+
+  return (
+    <PostFeed
+      posts={feedArray}
+      hasNextPage={hasNextPage}
+      loading={isLoading || isFetchingNextPage}
+      error={isLoadingError || isFetchNextPageError}
+      fetchNextPage={fetchNextPage}
+      refetch={refetch}
+    />
+  );
+}
+
+function Replies({ username }: { username: string }) {
+  const {
+    data: feed,
+    fetchNextPage,
+    hasNextPage,
+    isLoading,
+    isLoadingError,
+    isFetchingNextPage,
+    isFetchNextPageError,
+    refetch,
+  } = trpc.user.replies.useInfiniteQuery(
+    { username, maxResults: 15 },
+    { getNextPageParam: (lastPage) => lastPage.nextCursor },
+  );
+  const [feedArray, setFeedArray] = useState<PostResolved[]>([]);
+
+  useEffect(() => {
+    setFeedArray((feed?.pages ?? []).flatMap((page) => page.posts));
+  }, [feed, username]);
+
+  return (
+    <PostFeed
+      posts={feedArray}
+      hasNextPage={hasNextPage}
+      loading={isLoading || isFetchingNextPage}
+      error={isLoadingError || isFetchNextPageError}
+      fetchNextPage={fetchNextPage}
+      refetch={refetch}
+    />
+  );
+}
+
 export default function Page({
   params: { username },
 }: {
   params: { username: string };
 }) {
   const { data: user } = trpc.user.username.useQuery({ username });
-  const { data: posts } = trpc.post.search.useQuery({
-    from: username,
-    reply: false,
-  });
 
   if (!user) {
     return (
@@ -49,40 +115,23 @@ export default function Page({
         </article>
       </BackHeader>
       <ProfileCard
-        name={user.name}
-        username={username}
-        bio={user.bio}
-        following={user.following}
-        followers={user.followers}
+        {...user}
         coverImage="https://picsum.photos/seed/picsum/1280/720"
-        profileImage={user.image}
-        location={user.location}
-        createdAt={user.registered}
-        verified={true}
         isOwner={true}
         isFollowing={false}
       />
       <Tabs defaultValue={ActiveTab.Posts} className="mt-3">
-        <TabsList>
+        <TabsList className="static">
           <TabsTrigger value={ActiveTab.Posts}>Posts</TabsTrigger>
           <TabsTrigger value={ActiveTab.Replies}>Replies</TabsTrigger>
           <TabsTrigger value={ActiveTab.Media}>Media</TabsTrigger>
           <TabsTrigger value={ActiveTab.Likes}>Likes</TabsTrigger>
         </TabsList>
-        <Separator />
         <TabsContent value={ActiveTab.Posts}>
-          <div className="mb-4 flex flex-col">
-            {posts ? (
-              posts.results.map((post, i) => (
-                <React.Fragment key={i}>
-                  <Post {...post} />
-                  <Separator />
-                </React.Fragment>
-              ))
-            ) : (
-              <Spinner size={30} />
-            )}
-          </div>
+          <Posts username={username} />
+        </TabsContent>
+        <TabsContent value={ActiveTab.Replies}>
+          <Replies username={username} />
         </TabsContent>
       </Tabs>
     </div>
