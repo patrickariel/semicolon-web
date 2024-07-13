65274e60aefc7bc78a51a977c19b42082f1fb470
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-07-13 20:21:50 +0700

    fix(web): weird scroll behavior on route change

---
+++ apps/web/app/(columns)/(user)/[username]/(profile)/layout.tsx
@@ -0,0 +1,51 @@
+import { BackHeader } from "@/components/back-header";
+import ProfileCard from "@/components/profile-card";
+import ScrollTop from "@/components/scroll-top";
+import { TabsList, TabsLink } from "@/components/tabs-link";
+import { trpc } from "@/lib/trpc";
+import { auth } from "@semicolon/auth";
+import _ from "lodash";
+import React from "react";
+
+export default async function Page({
+  params: { username },
+  children,
+}: {
+  params: { username: string };
+  children: React.ReactNode;
+}) {
+  const user = await trpc.user.username.query({ username });
+  const session = await auth();
+
+  return (
+    <div className="flex min-h-full flex-col">
+      {/* INSANE bug: 
+      - https://github.com/vercel/next.js/discussions/64435#discussioncomment-9101547 
+      - https://github.com/vercel/next.js/issues/64441 */}
+      <ScrollTop />
+      <BackHeader>
+        <article className="flex flex-col">
+          <p className="text-xl font-semibold">{user.name}</p>
+          <p className="text-muted-foreground text-xs">
+            {`${new Intl.NumberFormat("en-US", {
+              notation: "compact",
+            }).format(user.posts)} posts`}
+          </p>
+        </article>
+      </BackHeader>
+      <ProfileCard
+        {...user}
+        coverImage="https://picsum.photos/seed/picsum/1280/720"
+        isOwner={session?.user?.username === user.username}
+        isFollowing={user.followed}
+      />
+      <TabsList className="static mt-1">
+        <TabsLink href={`/${username}`}>Posts</TabsLink>
+        <TabsLink href={`/${username}/replies`}>Replies</TabsLink>
+        <TabsLink href={`/${username}/media`}>Media</TabsLink>
+        <TabsLink href={`/${username}/likes`}>Likes</TabsLink>
+      </TabsList>
+      {children}
+    </div>
+  );
+}
---
+++ apps/web/app/(columns)/(user)/[username]/(profile)/replies/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { PostFeed } from "@/components/post-feed";
+import { trpc } from "@/lib/trpc-client";
+import type { PostResolved } from "@semicolon/api/schema";
+import _ from "lodash";
+import React, { useEffect, useState } from "react";
+
+export default function Page({
+  params: { username },
+}: {
+  params: { username: string };
+}) {
+  const {
+    data: feed,
+    fetchNextPage,
+    hasNextPage,
+    isLoading,
+    isLoadingError,
+    isFetchingNextPage,
+    isFetchNextPageError,
+    refetch,
+  } = trpc.user.replies.useInfiniteQuery(
+    { username, maxResults: 15 },
+    { getNextPageParam: (lastPage) => lastPage.nextCursor },
+  );
+  const [feedArray, setFeedArray] = useState<PostResolved[]>([]);
+
+  useEffect(() => {
+    setFeedArray((feed?.pages ?? []).flatMap((page) => page.posts));
+  }, [feed, username]);
+
+  return (
+    <PostFeed
+      posts={feedArray}
+      hasNextPage={hasNextPage}
+      loading={isLoading || isFetchingNextPage}
+      error={isLoadingError || isFetchNextPageError}
+      fetchNextPage={fetchNextPage}
+      refetch={refetch}
+    />
+  );
+}
