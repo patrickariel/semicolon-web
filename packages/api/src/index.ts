6f3ae9fbff95e9139aee1ab6ab3824de02029f99
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-07-16 18:18:28 +0700

    feat(api): export caller factory

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,7 +1,7 @@
 import { feed } from "./routes/feed";
 import { post } from "./routes/post";
 import { user } from "./routes/user";
-import { router } from "./trpc";
+import { router, t } from "./trpc";
 import { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
 
 export const appRouter = router({
@@ -11,6 +11,7 @@ export const appRouter = router({
 });
 
 export type AppRouter = typeof appRouter;
+export const createCaller = t.createCallerFactory(appRouter);
 export type RouterInput = inferRouterInputs<AppRouter>;
 export type RouterOutput = inferRouterOutputs<AppRouter>;
 export { createContext } from "./trpc";
