43e36311935d02914092c4b5f97cd901ad6f5766
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-07-17 00:56:43 +0700

    test: update user api tests

--- packages/api/src/__tests__/user.test.ts
+++ packages/api/src/__tests__/user.test.ts
@@ -1,15 +1,17 @@
 import { createCaller } from "..";
 import { type Session, update } from "@semicolon/auth";
 import { User, db } from "@semicolon/db";
+import { head } from "@vercel/blob";
 import { afterEach, beforeEach, describe, expect, test, vi } from "vitest";
 
 interface UserContext {
   user: User;
   session: Session;
-  caller: ReturnType<typeof createCaller>;
+  router: ReturnType<typeof createCaller>;
 }
 
 vi.mock("@semicolon/auth");
+vi.mock("@vercel/blob");
 
 function mockUpdateImplementation(session: Session) {
   return (data: Parameters<typeof update>[0]) => {
@@ -38,15 +40,15 @@ async function createUserContext(
     expires: expires.toISOString(),
   };
 
-  const caller = createCaller({ session });
+  const router = createCaller({ session });
 
   vi.mocked(update).mockImplementation(mockUpdateImplementation(session));
 
-  return { user, session, caller };
+  return { user, session, router };
 }
 
 beforeEach<UserContext>(async (context) => {
-  const { user, caller, session } = await createUserContext({
+  const { user, router, session } = await createUserContext({
     name: "John Smith",
     username: "john.smith",
     birthday: new Date(),
@@ -54,7 +56,7 @@ beforeEach<UserContext>(async (context) => {
   });
 
   context.user = user;
-  context.caller = caller;
+  context.router = router;
   context.session = session;
 });
 
@@ -65,42 +67,74 @@ afterEach(() => {
 describe("test user accounts", () => {
   test<UserContext>("find user by username", async ({
     user: { username },
-    caller,
+    router,
   }) => {
     expect(username).not.toBeNull();
-    const user = await caller.user.username({ username: username! });
-
+    const user = await router.user.username({ username: username! });
     expect(user).not.toBeNull();
   });
 
-  test<UserContext>("get self", async ({ caller }) => {
-    const user = await caller.user.me();
-
+  test<UserContext>("get self", async ({ router }) => {
+    const user = await router.user.me();
     expect(user).not.toBeNull();
   });
 
-  test<UserContext>("update user info", async ({ session, caller }) => {
-    await caller.user.update({ name: "Jane Smith" });
+  test<UserContext>("update user info", async ({ session, router }) => {
+    const name = "Jane Smith";
+    await router.user.update({ name });
+    expect((await router.user.me()).name).toBe(name);
+    expect(session.user?.name).toBe(name);
 
-    const user = await caller.user.me();
+    await router.user.update({ name: undefined });
+    expect((await router.user.me()).name).toBe(name);
+    expect(session.user?.name).toBe(name);
 
-    expect(user.name).toBe("Jane Smith");
-    expect(session.user?.name).toBe("Jane Smith");
+    vi.mocked(head).mockImplementation((url) =>
+      Promise.resolve({
+        url,
+        downloadUrl: `${url}?download=1`,
+        size: 100,
+        uploadedAt: new Date(),
+        pathname: "profilesv1/image.jpg",
+        contentType: "image/jpeg",
+        contentDisposition: 'attachment; filename="image.jpg"',
+        cacheControl: "public, max-age=31536000, s-maxage=300",
+      }),
+    );
+
+    const header = "https://example.com/image.jpg";
+    await router.user.update({ header });
+    expect((await router.user.me()).header).toBe(header);
+
+    await router.user.update({ header: undefined });
+    expect((await router.user.me()).header).toBe(header);
+    await router.user.update({ header: null });
+    expect((await router.user.me()).header).toBeNull();
+
+    const avatar = "https://example.com/picture.jpg";
+    await router.user.update({ avatar });
+    expect((await router.user.me()).image).toBe(avatar);
+    expect(session.user?.image).toBe(avatar);
+
+    await router.user.update({ avatar: undefined });
+    expect((await router.user.me()).image).toBe(avatar);
+    await router.user.update({ avatar: null });
+    expect((await router.user.me()).image).toBeNull();
+    expect(session.user?.image).toBeNull();
   });
 
   test("register flow", async () => {
     const { session } = await createUserContext({});
 
-    const caller = createCaller({ session });
-    await caller.user.register({
+    const router = createCaller({ session });
+    const data = {
       name: "Jane Smith",
       username: "jane.smith",
       birthday: new Date(),
-    });
+    };
+    await router.user.register(data);
 
-    const me = await caller.user.me();
-
-    expect(me.name).toBe("Jane Smith");
-    expect(session.user?.name).toBe("Jane Smith");
+    const { name, username, birthday } = await router.user.me();
+    expect({ name, username, birthday }).toStrictEqual(data);
   });
 });
