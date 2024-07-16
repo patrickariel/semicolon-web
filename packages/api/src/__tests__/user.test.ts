8fc67aa3737f2c8c892fbe72bac42f494d3915c1
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-07-16 23:36:22 +0700

    test: basic user tests

--- packages/api/src/__tests__/user.test.ts
+++ packages/api/src/__tests__/user.test.ts
@@ -1,20 +1,106 @@
 import { createCaller } from "..";
-import { db } from "@semicolon/db";
-import { expect, test } from "vitest";
+import { type Session, update } from "@semicolon/auth";
+import { User, db } from "@semicolon/db";
+import { afterEach, beforeEach, describe, expect, test, vi } from "vitest";
 
-test("find user by username", async () => {
-  const { username } = await db.user.create({
-    data: {
-      name: "John Smith",
-      username: "john.smith",
-      registered: new Date(),
-    },
+interface UserContext {
+  user: User;
+  session: Session;
+  caller: ReturnType<typeof createCaller>;
+}
+
+vi.mock("@semicolon/auth");
+
+function mockUpdateImplementation(session: Session) {
+  return (data: Parameters<typeof update>[0]) => {
+    if (session.user) {
+      session.user = {
+        ...session.user,
+        ...data.user,
+      };
+      return Promise.resolve(session);
+    } else {
+      return Promise.resolve(null);
+    }
+  };
+}
+
+async function createUserContext(
+  data: Parameters<typeof db.user.create>[0]["data"],
+): Promise<UserContext> {
+  const user = await db.user.create({ data });
+
+  const expires = new Date();
+  expires.setFullYear(expires.getFullYear() + 1);
+
+  const session = {
+    user,
+    expires: expires.toISOString(),
+  };
+
+  const caller = createCaller({ session });
+
+  vi.mocked(update).mockImplementation(mockUpdateImplementation(session));
+
+  return { user, session, caller };
+}
+
+beforeEach<UserContext>(async (context) => {
+  const { user, caller, session } = await createUserContext({
+    name: "John Smith",
+    username: "john.smith",
+    birthday: new Date(),
+    registered: new Date(),
   });
 
-  expect(username).not.toBeNull();
+  context.user = user;
+  context.caller = caller;
+  context.session = session;
+});
 
-  const caller = createCaller({ session: null });
-  const user = await caller.user.username({ username: username! });
+afterEach(() => {
+  vi.restoreAllMocks();
+});
 
-  expect(user).not.toBeNull();
+describe("test user accounts", () => {
+  test<UserContext>("find user by username", async ({
+    user: { username },
+    caller,
+  }) => {
+    expect(username).not.toBeNull();
+    const user = await caller.user.username({ username: username! });
+
+    expect(user).not.toBeNull();
+  });
+
+  test<UserContext>("get self", async ({ caller }) => {
+    const user = await caller.user.me();
+
+    expect(user).not.toBeNull();
+  });
+
+  test<UserContext>("update user info", async ({ session, caller }) => {
+    await caller.user.update({ name: "Jane Smith" });
+
+    const user = await caller.user.me();
+
+    expect(user.name).toBe("Jane Smith");
+    expect(session.user?.name).toBe("Jane Smith");
+  });
+
+  test("register flow", async () => {
+    const { session } = await createUserContext({});
+
+    const caller = createCaller({ session });
+    await caller.user.register({
+      name: "Jane Smith",
+      username: "jane.smith",
+      birthday: new Date(),
+    });
+
+    const me = await caller.user.me();
+
+    expect(me.name).toBe("Jane Smith");
+    expect(session.user?.name).toBe("Jane Smith");
+  });
 });
