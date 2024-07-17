61b2563254896ad240c9c8f73639be21a854dd11
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-07-18 00:27:51 +0700

    test(api): refactor tests into separate files

--- packages/api/src/__tests__/setup.ts
+++ packages/api/src/__tests__/setup.ts
@@ -1,10 +1,29 @@
+import { createUserContext, type UserContext } from "./utils";
 import { db } from "@semicolon/db";
-import { beforeEach } from "vitest";
+import { afterEach, beforeEach, vi } from "vitest";
 
-beforeEach(async () => {
+vi.mock("@semicolon/auth");
+vi.mock("@vercel/blob");
+
+afterEach(() => {
+  vi.restoreAllMocks();
+});
+
+beforeEach<UserContext>(async (context) => {
   await db.$transaction([
     db.like.deleteMany(),
     db.post.deleteMany(),
     db.user.deleteMany(),
   ]);
+
+  const { user, router, session } = await createUserContext({
+    name: "John Smith",
+    username: "john.smith",
+    birthday: new Date(),
+    registered: new Date(),
+  });
+
+  context.user = user;
+  context.router = router;
+  context.session = session;
 });
