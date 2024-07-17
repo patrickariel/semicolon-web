803026e624cca6f38c4fdfbe982a0c2f9f0e395d
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-07-18 01:28:46 +0700

    feat(ui): add auto-resizing text area

--- packages/ui/src/textarea.tsx
+++ packages/ui/src/textarea.tsx
@@ -1,12 +1,15 @@
 import { cn } from "@semicolon/ui/utils";
 import * as React from "react";
+import TextareaAutosize, {
+  type TextareaAutosizeProps,
+} from "react-textarea-autosize";
 
-export type TextareaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>;
+export { type TextareaAutosizeProps } from "react-textarea-autosize";
 
-const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaAutosizeProps>(
   ({ className, ...props }, ref) => {
     return (
-      <textarea
+      <TextareaAutosize
         className={cn(
           "border-input placeholder:text-muted-foreground focus-visible:ring-ring flex min-h-[60px] w-full rounded-md border bg-transparent px-3 py-2 text-sm shadow-sm focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50",
           className,
