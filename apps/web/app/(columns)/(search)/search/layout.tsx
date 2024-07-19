82540323f3e7d9ca097ecdfe18c99786dd9bc472
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-07-19 19:59:11 +0700

    feat(web): improve general responsiveness

--- apps/web/app/(columns)/(search)/search/layout.tsx
+++ apps/web/app/(columns)/(search)/search/layout.tsx
@@ -9,7 +9,7 @@ export default function Layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="relative z-0 flex w-full min-w-[257px] flex-col gap-1 md:max-w-[650px]">
+    <div className="relative z-0 flex w-full flex-col gap-1">
       <div className="sticky top-0 z-10 flex flex-col backdrop-blur-md">
         <BackHeader className="flex flex-row px-3">
           <SearchBar />
