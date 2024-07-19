3e34642a42019794510adc2cbbb8d89aa7910dfa
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-07-19 21:12:12 +0700

    refactor(web): change dummy trends

--- apps/web/components/trends.tsx
+++ apps/web/components/trends.tsx
@@ -3,34 +3,50 @@ import Link from "next/link";
 import React from "react";
 
 const trends = [
-  { rank: 1, topic: "bootcamp javascript", posts: "32K posts" },
-  { rank: 2, topic: "mern technology", posts: "30K posts" },
-  { rank: 3, topic: "how to learn next js", posts: "27K posts" },
-  { rank: 4, topic: "knex vs sequelize", posts: "25K posts" },
-  { rank: 5, topic: "next js or svelte?", posts: "24K posts" },
-  { rank: 6, topic: "praesent sed", posts: "12K posts" },
-  { rank: 7, topic: "sem commodo", posts: "15K posts" },
-  { rank: 8, topic: "fermentum orci id", posts: "10K posts" },
-  { rank: 9, topic: "amet mauris", posts: "11K posts" },
+  {
+    header: "Travel · Trending",
+    topic: "America Airlines",
+    footer: "32K posts",
+  },
+  {
+    header: "Trending in United States",
+    topic: "Only 22",
+    footer: "11.8K posts",
+  },
+  { header: "Travel · Trending", topic: "Southwest", footer: "9,783 posts" },
+  {
+    header: "Trending in United States",
+    topic: "Leave the World Behind",
+    footer: "2,980 posts",
+  },
+  { header: "Wrestling · Trending", topic: "Hulk Hogan", footer: "263K posts" },
+  {
+    header: "Trending in United States",
+    topic: "Wyndham Clark",
+    footer: "12K posts",
+  },
+  {
+    header: "Trending in United States",
+    topic: "Biggest IT",
+    footer: "102K posts",
+  },
 ];
 
 export function Trends() {
   return (
-    <Card>
-      <CardHeader>Indonesia trends</CardHeader>
+    <Card className="rounded-2xl">
+      <CardHeader className="pb-5 pt-4 text-lg font-black">
+        What{"'"}s happening
+      </CardHeader>
       <CardContent className="flex flex-col gap-5">
-        {trends.map((trend) => (
-          <Link
-            key={trend.rank}
-            href="#"
-            className="flex items-center justify-between"
-          >
+        {trends.map(({ header, topic, footer }, i) => (
+          <Link key={i} href="#" className="flex items-center justify-between">
             <div className="flex flex-col gap-1">
               <p className="text-muted-foreground align-bottom text-sm">
-                Topic
+                {header}
               </p>
-              <p className="font-bold">{trend.topic}</p>
-              <p className="text-muted-foreground text-sm">{trend.posts}</p>
+              <p className="font-bold">{topic}</p>
+              <p className="text-muted-foreground text-sm">{footer}</p>
             </div>
           </Link>
         ))}
