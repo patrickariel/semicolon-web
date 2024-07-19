42fcadef2a85082068d7f0e8916dec397387b03d
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-07-19 18:05:49 +0700

    feat(web): implement sidebar item focus state

--- apps/web/components/nav-item.tsx
+++ apps/web/components/nav-item.tsx
@@ -2,6 +2,7 @@ import { Button } from "@semicolon/ui/button";
 import { cn } from "@semicolon/ui/utils";
 import { LucideIcon } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 interface NavItemProps {
@@ -16,17 +17,24 @@ export function NavItem({
   children,
   className,
 }: Parameters<typeof Link>[0] & NavItemProps) {
+  const pathname = usePathname();
+
   return (
     <Button
       variant="ghost"
       asChild
       className={cn(
         "aspect-square h-auto shrink justify-center rounded-full p-1 lg:aspect-auto lg:min-h-14 lg:w-fit lg:justify-start lg:px-4 lg:py-2",
+        href === pathname ? "font-black" : "",
         className,
       )}
     >
       <Link href={href} className="flex items-center gap-4">
-        {Icon && <Icon className="size-7 flex-none" />}
+        {Icon && (
+          <Icon
+            className={`size-7 flex-none ${href === pathname ? "fill-foreground" : ""}`}
+          />
+        )}
         {label && (
           <p className="hidden align-middle text-xl lg:block">{label}</p>
         )}
