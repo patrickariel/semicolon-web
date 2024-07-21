3ead6462ce2f792c82ad5725638e2576d8a4c6e1
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-07-22 01:53:24 +0700

    feat(web): adjust font size of verify page

--- apps/web/app/flow/verify/page.tsx
+++ apps/web/app/flow/verify/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
       <Mail size={100} className="flex-none" />
       <div className="flex h-full flex-col items-start justify-center gap-2">
         <CardHeader className="flex flex-row items-center gap-7 self-center">
-          <CardTitle className="self-center text-nowrap text-2xl sm:text-5xl">
+          <CardTitle className="self-center text-nowrap text-2xl sm:text-4xl">
             Check your email
           </CardTitle>
         </CardHeader>
