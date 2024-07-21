585dd1db5ad2f81e0da4c92e47ae7ba6c5bfe222
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-07-21 20:48:09 +0700

    test(web): add tests for back button and nav item

---
+++ apps/web/__tests__/components/back-header.test.tsx
@@ -0,0 +1,24 @@
+import { BackHeader } from "@/components/back-header";
+import { render, screen } from "@testing-library/react";
+import { useRouter } from "next/navigation";
+import { expect, test, vi } from "vitest";
+import { mock } from "vitest-mock-extended";
+
+test("back-header", () => {
+  vi.mock("next/navigation");
+  const routerMocked = mock<ReturnType<typeof useRouter>>();
+  vi.mocked(useRouter).mockReturnValue(routerMocked);
+
+  render(<BackHeader />);
+
+  const backButton = screen.getByLabelText("Go back");
+  expect(backButton).toBeDefined();
+
+  vi.spyOn(window.history, "length", "get").mockReturnValue(0);
+  backButton.click();
+  expect(routerMocked.push).toBeCalledWith("/home");
+
+  vi.spyOn(window.history, "length", "get").mockReturnValue(3);
+  backButton.click();
+  expect(routerMocked.back).toBeCalled();
+});
